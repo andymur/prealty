e258e085520a5a8ab7b61f5dff1f7fcf88226c1e
andymurh <andymurh@gmail.com>
2020-09-05 10:33:14 +0200

    Setup as a systemd service

---
+++ daemoncron.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import schedule
+import time
+from daemonize import Daemonize
+import os
+import sys
+
+def job():
+    print("I'm working...")
+    f = open("/home/antti/dom.txt", "a")
+    f.write(time.strftime("%Y-%m-%d %H:%M:%S") + "\n")
+    f.close()
+
+schedule.every(5).seconds.do(job)
+
+
+def main():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+     myname=os.path.basename(sys.argv[0])
+     pidfile='/tmp/%s' % myname
+     daemon = Daemonize(app=myname,pid=pidfile, action=main)
+     daemon.start()
---
+++ pycron.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+script='/home/antti/workspace/prealty/cron.py'
+/usr/bin/python3 $script &
