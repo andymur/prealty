ca35d48a8ef54a301a71c1890b3721f12e0f7748
Andrei Murashko <andrei.murashko@360t.com>
2020-09-08 14:46:27 +0200

    Not requesting server if file is there

--- template.py
+++ template.py
@@ -19,43 +19,60 @@ else:
     baseurl = rooturl + '/prodazha-kvartiry-belgorod-c310?PublicationTimeRange=OneDay'
 
 
-def construct_whole_page_filename(pagenumber):
-    return "belgorod-{0}-{1}.html".format(time.strftime("%Y-%m-%d"), pagenumber)
+def construct_whole_page_filename(basedir, pagenumber):
+    return "{0}belgorod-{1}-{2}.html".format(basedir, time.strftime("%Y-%m-%d"), pagenumber)
 
-def construct_json_filename(pagenumber):
-    return "belgorod-{0}-{1}.json".format(time.strftime("%Y-%m-%d"), pagenumber)
+def construct_json_filename(basedir, pagenumber):
+    return "{0}belgorod-{1}-{2}.json".format(basedir, time.strftime("%Y-%m-%d"), pagenumber)
 
 def store_content(filename, content):
     with open(filename, "w") as f:
         f.write(content)
 
+def get_content(filename):
+    with open(filename) as f:
+        return f.read()
+
+def get_json(page_content):
+    script_content = page_content.split("<script")[8]
+    return script_content[27:-26]
+
 def get_page_number(link):
     m = re.search("Page=(\d+)", link)
     return 1 if not m else int(m.group(1))
 
+def get_base_url(json_content):
+    page_object = json.loads(json_content)
+    if 'nextLink' in page_object['metaInformationState']['current']:
+        return rooturl + page_object['metaInformationState']['current']['nextLink']
+    else:
+        return None
+
 if __name__ == "__main__":
     while True:
-        print("requesting url...", baseurl)
         page_number = get_page_number(baseurl)
-	#TODO: no need to make requests if file exists
-        page = requests.get(baseurl, headers={'User-Agent': 'Mozilla'})
         
-        page_file = basedir + construct_whole_page_filename(page_number)
-        json_file = basedir + construct_json_filename(page_number)
-        print("page file: " + page_file)
+        page_file = construct_whole_page_filename(basedir, page_number)
+        json_file = construct_json_filename(basedir, page_number)
+        page_was_uploaded = False
 
         if not os.path.isfile(page_file):
-            store_content(page_file, page.text)
-        
-        script_content = page.text.split("<script")[8]
-        json_content = script_content[27:-26]
+            print("uploading new page...")
+            page = requests.get(baseurl, headers={'User-Agent': 'Mozilla'})
+            page_content = page.text
+            print("storing html file..." + page_file)
+            store_content(page_file, page_content)
+            page_was_uploaded = True
+        else:
+            page_content = get_content(page_file)
 
-        if not os.path.isfile(json_file):
+        json_content = get_json(page_content)
+
+        if not os.path.isfile(json_file) or page_was_uploaded:
+            print("storing json file..." + json_file)
             store_content(json_file, json_content)
 
-        page_object = json.loads(json_content)
-        if 'nextLink' in page_object['metaInformationState']['current']:
-            baseurl = rooturl + page_object['metaInformationState']['current']['nextLink']
-        else:
-            print('last page')
+        baseurl = get_base_url(json_content)
+        if not baseurl:
+            print("end of work...")
             break
